e1af856ecb4c11cbb586d003d9534586468fdd09
mdrennan91 <mitcheldrennan@gmail.com>
2024-02-05 16:28:41 -0800

    Styling adjustments, fixed mobile view nav menu

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     var hamburger = document.querySelector('.hamburger');
     var navbar = document.querySelector('.navbar');
-
+    console.log('hamburger: ', hamburger)
+    console.log('navbar: ', navbar.classList)
     hamburger.addEventListener('click', function() {
         navbar.classList.toggle('open');
-        // Toggle between '☰' for closed and '×' for open states
         hamburger.textContent = navbar.classList.contains('open') ? '' : '☰';
     });
 });
